37b0baf243f1f4cff724489f3b366f5dee5318d4
WarzoneEnjoyer-Tools <tailtop9@outlook.com>
2026-07-14 17:51:13 +0930

    CloseMW.bat

--- CloseMW.bat
+++ CloseMW.bat
@@ -1,5 +1,4 @@
 @echo off
-:: Check if the script is already running as Admin
 NET SESSION >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo Requesting administrative privileges...
@@ -7,7 +6,6 @@ IF %ERRORLEVEL% NEQ 0 (
     exit /b
 )
 
-:: Your code runs here once Admin permissions are granted
 echo Closing Modern Warfare...
 taskkill /f /im ModernWarfare.exe 2>nul
 
